570582b0238bc9e287656d2e1ea38356cd6f3ae4
jordanzet <jordanzet@gmail.com>
2016-07-12 22:28:46 -0500

    devel spirit

--- webspirit/urls.py
+++ webspirit/urls.py
@@ -1,21 +1,32 @@
-"""webspirit URL Configuration
+# -*- coding: utf-8 -*-
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
+import spirit.urls
+
+# Override admin login for security purposes
+from django.contrib.auth.decorators import login_required
+admin.site.login = login_required(admin.site.login)
+
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'example.views.home', name='home'),
+    # url(r'^example/', include('example.foo.urls')),
+
+    url(r'^', include(spirit.urls)),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
